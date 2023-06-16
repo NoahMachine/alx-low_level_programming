71734ab62b98abfa9c11f27d11c248cae69073ac
NoahMachine <carrelsimo33@gmail.com>
2023-06-16 07:53:13 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,8 +9,8 @@ int main(void)
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of a int: %zu byte(s)\n", sizeof(int));
 printf("Size of long int: %zu byte(s)\n", sizeof(long int));
-print("Size of long long int: %zu byte(s)", sizeof(long long int));
-printf("Size of a float %zu byte(s)\n", sizeof(float));
+printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
 return (0);
 }
 "6-size.c"[New] 20L, 487C written
