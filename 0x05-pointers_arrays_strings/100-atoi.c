63d22e7b6d03e952b888c1605c4679e4a72c7a50
NoahMachine <carrelsimo331@gmail.com>
2023-06-26 02:39:38 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,7 +13,7 @@ int isi = 0;
 while (s[c])
 {
 if (s[c] == 45)
-{	
+{
 min *= -1;
 }
 while (s[c] >= 48 && s[c] <= 57)
@@ -23,7 +23,7 @@ ni = (ni * 10) + (s[c] - '0');
 c++;
 }
 if (isi == 1)
-{	
+{
 break;
 }
 c++;
