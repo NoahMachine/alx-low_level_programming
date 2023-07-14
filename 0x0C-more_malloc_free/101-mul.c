d64856b7b07d748956bd8b523c1e9753121b891a
NoahMachine <carrelsimo331@gmail.com>
2023-07-13 23:31:12 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #define ERR_MSG "Error"
 /**
-*is_digit - checks if a string contains a non-digit char
+*is_digit - Checks if a string contains a non-digit char
 *@s: string to be evaluated
 *Return: 0 if a non-digit is found, 1 otherwise
 */
@@ -19,9 +19,9 @@ i++;
 return (1);
 }
 /**
-*_strlen - returns the length of a string
+*_strlen - main the length of a string
 *@s: string to evaluate
-*Return: the length of the string
+*Return: The length of the string
 */
 int _strlen(char *s)
 {
@@ -44,7 +44,7 @@ exit(98);
 *main - multiplies two positive numbers
 *@argc: number of arguments
 *@argv: array of arguments
-*Return: always 0 (Success)
+*Return: Always 0 (Success)
 */
 int main(int argc, char *argv[])
 {
@@ -68,7 +68,7 @@ carry = 0;
 for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 {
 digit2 = s2[len2] - '0';
-carry += result[len1 + len2 + 1] + (digit1 * digit2);
+carry += result[len1 + len2 + 1] + (digit1)*(digit2);
 result[len1 + len2 + 1] = carry % 10;
 carry /= 10;
 }
